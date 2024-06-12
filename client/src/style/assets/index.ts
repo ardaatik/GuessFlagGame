b6a2469f6393b365fde445254ccdd6aa2fe4d60d
Arda <ardaatikcbu@gmail.com>
2024-06-12 22:47:09 +0300

    Added necessary assets

---
+++ client/src/style/assets/index.ts
@@ -0,0 +1,17 @@
+/// <reference types="vite-plugin-svgr/client" />
+
+export { default as Icon1v1 } from "./1v1.svg?react";
+export { default as IconClose } from "./close.svg?react";
+export { default as IconContentCopy } from "./content_copy.svg?react";
+export { default as IconCross } from "./cross.svg?react";
+export { default as IconCustomize } from "./customize.svg?react";
+export { default as IconDownArrow } from "./keyboard_arrow_down.svg?react";
+export { default as IconLeftArrow } from "./keyboard_arrow_left.svg?react";
+export { default as IconMistake } from "./mistake.svg?react";
+export { default as IconAmount } from "./numbers.svg?react";
+export { default as IconPlay } from "./play.svg?react";
+export { default as IconRefresh } from "./refresh.svg?react";
+export { default as IconRegion } from "./region.svg?react";
+export { default as IconTime } from "./time.svg?react";
+export { default as IconUser } from "./user.svg?react";
+export { default as IconVolume } from "./volume.svg?react";
