ebc15f8bb73f2cf49fc7456589f8f924b4cafcef
Arda <ardaatikcbu@gmail.com>
2024-06-23 23:20:32 +0300

    feat: Add Lobby component for the multiplayer lobby screen

---
+++ client/src/component/Multiplayer/Lobby.tsx
@@ -0,0 +1,38 @@
+import { IconUser } from "@/style/assets/images";
+import styles from "@/style/scss/Lobby.module.scss";
+import Header from "../Header";
+import CardContainer from "../UI/CardContainer";
+import CopyButton from "../UI/CopyButton";
+import Loading from "../UI/Loading";
+
+interface LobbyProps {
+	room: string;
+}
+
+function Lobby({ room }: LobbyProps) {
+	return (
+		<CardContainer>
+			<Header />
+			<div className={styles.waitingWrapper}>
+				<div className={styles.roomCode}>
+					<span className={styles.roomCodeText}>Room Code: </span>
+					<span className={styles.roomCodeTextCode}>{room}</span>
+					<CopyButton className={styles.roomCodeCopyButton} value={room} />
+				</div>
+				<div className={styles.players}>
+					<div className={styles.player}>
+						<span className={styles.playerText}>You</span>
+						<IconUser className={styles.iconUser} />
+					</div>
+					<span className={styles.textVs}>vs</span>
+					<div className={styles.player}>
+						<span className={styles.playerText}>Opponent</span>
+						<Loading type="dot-flashing" />
+					</div>
+				</div>
+			</div>
+		</CardContainer>
+	);
+}
+
+export default Lobby;
