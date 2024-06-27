694d5ba77f717e6ce64ef87c38c5f1c62709a766
Arda <ardaatikcbu@gmail.com>
2024-06-27 23:02:19 +0300

    Add TextButton component for displaying a styled text button

---
+++ client/src/component/UI/TextButton.tsx
@@ -0,0 +1,20 @@
+import styles from "@/style/scss/TextButton.module.scss";
+
+interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+	isActive: boolean;
+}
+
+export default function TextButton(props: Props) {
+	const { isActive, className, children, ...rest } = props;
+
+	return (
+		<button
+			className={`${styles.button} ${
+				isActive ? styles.active : ""
+			} ${className}`}
+			{...rest}
+		>
+			{children}
+		</button>
+	);
+}
