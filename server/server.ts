267e88e613240514f49bad53cd5e59e772f0704f
Arda <ardaatikcbu@gmail.com>
2024-07-02 01:46:59 +0300

    Added cross-env for changed startup for production

--- server/server.ts
+++ server/server.ts
@@ -1,6 +1,7 @@
 import cors from "cors";
 import express from "express";
 import { createServer } from "http";
+import path from "path";
 import configureSockets from "./sockets";
 
 const app = express();
@@ -10,10 +11,11 @@ const port = process.env.PORT || 3000;
 
 app.use(
 	cors({
-		origin: "*",
+		origin:
+			process.env.NODE_ENV === "development" ? "http://localhost:3000" : "",
 	})
 );
-// app.use(express.static(path.join(__dirname, "../client/dist")));
+app.use(express.static(path.join(__dirname, "./client-build")));
 
 console.log(`Attempting to run server on port ${port}`);
 
@@ -21,6 +23,6 @@ const server = createServer(app);
 configureSockets(server);
 
 // Handles any requests that don't match the ones above
-// app.get("*", (req, res) => {
-// 	res.sendFile(path.join(__dirname, "../client/dist/index.html"));
-// });
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "./client-build/index.html"));
+});
