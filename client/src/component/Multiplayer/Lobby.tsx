e22ddab070db5e55092a2cd42d6602e2e38bd46b
Arda <ardaatikcbu@gmail.com>
2024-07-02 01:48:54 +0300

    Update Lobby component UI for multiplayer mode

--- client/src/component/Multiplayer/Lobby.tsx
+++ client/src/component/Multiplayer/Lobby.tsx
@@ -12,22 +12,26 @@ interface LobbyProps {
 function Lobby({ room }: LobbyProps) {
 	return (
 		<CardContainer>
-			<Header />
-			<div className={styles.waitingWrapper}>
-				<div className={styles.roomCode}>
-					<span className={styles.roomCodeText}>Room Code: </span>
-					<span className={styles.roomCodeTextCode}>{room}</span>
-					<CopyButton className={styles.roomCodeCopyButton} value={room} />
-				</div>
-				<div className={styles.players}>
-					<div className={styles.player}>
-						<span className={styles.playerText}>You</span>
-						<IconUser className={styles.iconUser} />
+			<Header title="Lobby" />
+			<div className={styles.lobbyWrapper}>
+				<div className={styles.waitingWrapper}>
+					<div className={styles.roomCode}>
+						<span className={styles.roomCodeText}>Room Code</span>
+						<div className={styles.roomCodeBottom}>
+							<span className={styles.roomCodeTextCode}>{room}</span>
+							<CopyButton className={styles.roomCodeCopyButton} value={room} />
+						</div>
 					</div>
-					<span className={styles.textVs}>vs</span>
-					<div className={styles.player}>
-						<span className={styles.playerText}>Opponent</span>
-						<Loading type="dot-flashing" />
+					<div className={styles.players}>
+						<div className={`${styles.containerPlayer} ${styles.container}`}>
+							<span className={styles.containerText}>You</span>
+							<IconUser className={styles.iconUser} />
+						</div>
+						<span className={styles.textVs}>vs</span>
+						<div className={`${styles.containerOpponent} ${styles.container}`}>
+							<span className={styles.containerText}>Opponent</span>
+							<Loading type="dot-flashing" />
+						</div>
 					</div>
 				</div>
 			</div>
